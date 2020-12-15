34c3643eb759cf6a5812ce37748c184789a43f78
Majlo <milosz.karlowicz@gmail.com>
2020-12-15 23:56:04 +0100

    Clear unwanted classes

--- src/main/java/pl/put/poznan/transformer/rest/BuildingController.java
+++ src/main/java/pl/put/poznan/transformer/rest/BuildingController.java
@@ -10,19 +10,35 @@ import pl.put.poznan.transformer.model.Level;
 import pl.put.poznan.transformer.model.Location;
 import pl.put.poznan.transformer.model.Room;
 
-
+/**
+ * BuildingController is class which enables to create REST communications.
+ * It also provides logging of all requests on INFO and DEBUG layer.
+ */
 @RestController
 public class BuildingController {
 
+    /**
+     * Enables logging.
+     */
     private final Logger logger = LoggerFactory.getLogger(BuildingController.class);
 
-
+    /**
+     * Simple testing if your JSON file is correct.
+     * @param building Building to be tested
+     * @return exactly the same building as provided as argument if correct
+     */
     @GetMapping("/test_my_building")
     Building returnSameBuilding(@RequestBody Building building) {
         logger.debug("Request for building check on " + building.getName());
         return building;
     }
 
+    /**
+     * Calculates area of specific Location
+     * @param id id of location to be examined
+     * @param building Building on which calculations are performed.
+     * @return area of Location provided as argument
+     */
     @GetMapping("/area/{id}")
     Answer returnArea(@PathVariable long id, @RequestBody Building building) {
         AreaVisitor areaVisitor = new AreaVisitor(id);
@@ -31,6 +47,12 @@ public class BuildingController {
         return new Answer(String.valueOf(areaVisitor.getArea()));
     }
 
+    /**
+     * Calculates volume of specific Location
+     * @param id id of Location to be examined
+     * @param building Building on which calculations are performed
+     * @return volume of Location provided as argument
+     */
     @GetMapping("/volume/{id}")
     Answer returnVolume(@PathVariable long id, @RequestBody Building building){
         VolumeVisitor volumeVisitor = new VolumeVisitor(id);
@@ -39,6 +61,12 @@ public class BuildingController {
         return new Answer(String.valueOf(volumeVisitor.getVolume()));
     }
 
+    /**
+     * Calculates light power per area unit of specific Location
+     * @param id id of Location to be examined
+     * @param building Building on which calculations are performed
+     * @return light power per area unit of Location provided as argument
+     */
     @GetMapping("/light/{id}")
     Answer returnLight(@PathVariable long id, @RequestBody Building building) {
         LightVisitor lightVisitor = new LightVisitor(id);
@@ -47,6 +75,12 @@ public class BuildingController {
         return new Answer(String.valueOf(lightVisitor.getLight()));
     }
 
+    /**
+     * Calculates power used on heating per volume unit of specific Location
+     * @param id id of Location to be examined
+     * @param building Building on which calculations are performed
+     * @return power used on heating per volume of Location provided as argument
+     */
     @GetMapping("/heating/{id}")
     Answer returnHeating(@PathVariable long id, @RequestBody Building building) {
         HeatingVisitor heatingVisitor = new HeatingVisitor(id);
@@ -55,6 +89,13 @@ public class BuildingController {
         return new Answer(String.valueOf(heatingVisitor.getHeating()));
     }
 
+    /**
+     * Provides list of Locations which heating is over parameter provided
+     * as argument
+     * @param heating heating power on which Location is overheating
+     * @param building Building on which calculations are performed
+     * @return list of Locations meeting the conditions
+     */
     @GetMapping("/overheating_locations/{heating}")
     Location[] returnOverheatingLocations(@PathVariable float heating, @RequestBody Building building) {
         OverheatingLocationsVisitor visitor = new OverheatingLocationsVisitor(heating);
@@ -63,6 +104,11 @@ public class BuildingController {
         return visitor.getOverheatedLocations();
     }
 
+    /**
+     * Generates simple building Location which can be further used to
+     * check others methods behavior
+     * @return Building witch 2 Levels and 6 Rooms
+     */
     @GetMapping("/sample_building")
     Building returnSampleBuilding() {
         Room room1 = new Room(5671, "Kitchen", 25, 140,
